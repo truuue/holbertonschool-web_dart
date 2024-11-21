6efd331f4687b9a8e7647db64f6b2e65abb13884
truuue <noahvernhet@gmail.com>
2024-11-21 21:59:52 +0100

    6. Positive anything is better than negative nothing

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int n = int.parse(args[0]);
+
+  if (n > 0) {
+    print("${n} is positive");
+  } else if (n < 0) {
+    print("${n} is negative");
+  } else {
+    print("${n} is zero");
+  }
+}
