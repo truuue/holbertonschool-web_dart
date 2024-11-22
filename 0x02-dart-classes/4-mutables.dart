be2deb35d1faa843f09555865c407880912fe457
truuue <noahvernhet@gmail.com>
2024-11-22 10:51:57 +0100

    4. Mutables

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,27 @@
+class Password {
+  String _password = "";
+
+  Password({String password = ""}) {
+    _password = password;
+  }
+
+  String get password => _password;
+
+  set password(String password) {
+    _password = password;
+  }
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+    if (!RegExp(r'(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])').hasMatch(_password)) {
+      return false;
+    }
+    return true;
+  }
+
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
